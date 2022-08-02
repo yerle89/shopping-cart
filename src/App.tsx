8dd83019d7cac54f015eb5f5240a1dab462d1a1a
yerle89 <martinyerle@gmail.com>
2022-08-02 16:46:44 +0200

    nav bar - routes - cart button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+import { Home } from "./pages/Home"
+import { Store } from "./pages/Store"
+import { About } from "./pages/About"
+import { Navbar } from "./components/Navbar"
+import './App.css';
+
+
 function App() {
-  return <h1> HELLO!! </h1>
-    
+  return (
+  <>
+  <Navbar />
+    <Container className="mb-4"> 
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="/store" element={ <Store/> } />
+        <Route path="/about" element={ <About/> } />
+          HELLO!! 
+      </Routes>
+    </Container>
+  </>
+  )
 }
 
 export default App;
